79301ff93eaa05462d83665f2219c179d6a1b0ca
KorecRay <ray0823.qiu@gmail.com>
2023-10-18 20:39:31 +0800

    !add show hide to banner !add scroll to main page

--- contexts/effects.js
+++ contexts/effects.js
@@ -1,20 +1,29 @@
 var specialCharArray = [
+    'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'l', 
+    'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+    'y', 'z', 'A', 'B', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
+    'W', 'X', 'Y', 'Z', 'Y', 'Z', 
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-',
     '+', '=', '[', ']', '{', '}', '|', ';', ':', "'", '"', ',',
     '.', '<', '>', '?', '/'
 ];
-export class _Text {
+
+export class _text {
     constructor() {
         this.enable = false;
         this.count = 0;
     }
 
-    glitch(id, duration) {
+    garbled(id, duration, rate) {
 
-        if(!id || !duration) {
-            throw new Error ("Not enough arguments at glitch.Text()");
+        if (!id || !duration) {
+            throw new Error("Not enough arguments at garbled.Text()");
         }
-
+        if(!rate) {
+            rate = 0.5;
+        }
         let stop = false;
         let textdom = document.getElementById(id);
         let text = textdom.innerText;
@@ -22,12 +31,12 @@ export class _Text {
         let charArr = text.split('');
 
         animation();
-        function animation () {
-            if(stop) {
+        function animation() {
+            if (stop) {
                 return;
             }
-            for(let i = 0; i < charArr.length; i++) {
-                if(Math.random() < 4) {
+            for (let i = 0; i < charArr.length; i++) {
+                if (Math.random() < rate && text[i] != ' ') {
                     charArr[i] = specialCharArray[Math.floor(Math.random() * specialCharArray.length)];
                 }
                 textdom.innerText = charArr.join('');
@@ -36,9 +45,17 @@ export class _Text {
                 animation();
             }, 100);
         }
-        setTimeout(function(){
-            stop = true;
-            textdom.innerText = oritext;
-        }, duration);
+        if (duration != "infinite") {
+            setTimeout(function () {
+                stop = true;
+                textdom.innerText = oritext;
+            }, duration);
+        }
+    }
+}
+
+export class _img {
+    constructor() {
+        
     }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,27 @@
-import {_Text} from "../../contexts/effects.js"
+import { _text, _img } from "../../contexts/effects.js"
 
-const Text = new _Text();
+const Text = new _text();
 
-Text.glitch("banner-text1", 1500);
-Text.glitch("banner-text2", 1500);
+Text.garbled("banner-text1", 1500, 0.5);
+Text.garbled("banner-text2", 1500, 0.5);
+function welcomeblink() {
+    Text.garbled("welcome-text", 3000, 0.01);
+    setTimeout(() => {
+        welcomeblink()
+    }, 10000);
+}
+welcomeblink()
+
+const fadeDiv = document.getElementById("banner");
+
+window.addEventListener('scroll', function () {
+    const scrollHeight = window.scrollY;
+
+    const triggerHeight = 100;
+
+    if (scrollHeight > triggerHeight) {
+        fadeDiv.style.opacity = '0';
+    } else {
+        fadeDiv.style.opacity = '1';
+    }
+});
